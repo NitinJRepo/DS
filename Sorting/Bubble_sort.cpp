4b6f1c55cb8ceb12d09ceb206096a1c27616c00a
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-08-24 19:19:43 +0530

    Added bubble sort

---
+++ Sorting/Bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#define   SIZE	5
+
+void swap(int &a, int &b)
+{
+	int temp= 0;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+int main(){
+	int data[SIZE],i,j,temp;
+
+	printf("\n ENTER THE ELEMENTS IN THE LIST : \n");
+	for(i=0;i<SIZE;i++)
+		scanf("%d",&data[i]);  //EX:10,4,6,2,3
+
+	printf("\n THE SORTED LIST IS :\n");
+
+	for(i=0;i<SIZE-1;i++)
+		for(j=0;j<SIZE-i-1;j++)
+			if(data[j]>data[j+1])
+			{
+			    swap(data[j],data[j+1]);
+			}
+
+	for(i=0;i<SIZE;i++)
+	  printf("\t%d",data[i]);
+
+}
